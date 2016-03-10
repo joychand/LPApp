20e65e687b16c410d76d2045745912c47ebd3e97
nj.singh@nic.in <Ntombi80>
2016-03-10 17:16:02 +0530

    ui-router

--- www/js/app.js
+++ www/js/app.js
@@ -3,10 +3,49 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app =angular.module('LPApp', ['ionic'])
+app.config(function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/')
 
-.run(function($ionicPlatform) {
+  $stateProvider.state('app', {
+    abstract: true,
+    url: '/todos',
+    //views: {
+     // todos: {
+        templateUrl: 'main.html'
+     // }
+   // }
+
+  })
+  $stateProvider.state('app.todos', {
+    url: '',
+    //views: {
+     // todos: {
+        templateUrl: 'todos.html',
+        controller: 'TodosCtrl'
+    // }
+    //}
+  })
+
+  $stateProvider.state('help', {
+    url: '/help',
+    views: {
+      help: {
+        templateUrl: 'help.html'
+     }
+   }
+  })
+    });
+   app.controller('TodosCtrl', function($scope) {
+      $scope.todos = [
+        {title: "Take out the trash"},
+        {title: "Do laundry"},
+        {title: "Start cooking dinner"}
+      ]
+    });
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
